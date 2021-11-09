a90fcd14d26916a2d9bcd9f90d496130108ea158
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-11-09 17:47:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int LastNumber = 9;
+
+            //Goes through the number 1 - 9
+            for (int i = 1; i <= LastNumber; i++)
+            {
+                //Forms the pattern 1, 22, 333
+                for (int x = 1; x <= i; x++)
+                {
+                    //Write() instead of WriteLine() because
+                    //WriteLine prints each number under each other 
+                    Console.Write(i); 
+                }
+                //Prints all the number patterns from 1 - 9
+                Console.WriteLine();
+            }
+           
+        Console.ReadLine();
+        }
+    }
+}
